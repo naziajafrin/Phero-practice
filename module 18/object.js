506842b5b7be325c42b29728f598b4fd6ce38d33
Nazia <nazia.jafrin1@gmail.com>
2023-12-20 22:46:10 +0600

    class 18-5

---
+++ module 18/object.js
@@ -0,0 +1,18 @@
+var student1 = {
+  id:115, name:'Abir Adnan', makrs:81, phone: 98984357
+};
+
+var mobile = {
+  color:'black',
+  price:19000,
+  screenSize:5,
+  storage:'10gb'
+}
+
+var book = {
+  title:'Start with why',
+  author:'Simon Sinek',
+  price:200,
+  pages:290,
+  publisher:'penguin'
+}
