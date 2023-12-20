6f3630ad8ba1cc69c7b91e13a39bd7746c4d47a6
Nazia <nazia.jafrin1@gmail.com>
2023-12-20 21:30:09 +0600

    class 18-4

--- module 18/multiple-parameter.js
+++ module 18/multiple-parameter.js
@@ -1,1 +1,16 @@
- 
+function addTwoNumbers(num1, num2){
+  console.log(num1, num2);
+  var total = num1 + num2;
+  return total;
+} 
+
+var result = addTwoNumbers(5,10);
+console.log('addresult',result);
+
+function multiplyNumbers(num1,num2){
+  console.log(num1, num2);
+  var total = num1 * num2;
+  return total;
+}
+var result = multiplyNumbers(45, 50);
+console.log("multiplication", result);
