5c1735d043518d6293525ec0337e307e1263fa52
Nazia <nazia.jafrin1@gmail.com>
2023-12-20 23:28:03 +0600

    class 18-7

---
+++ module 18/switch.js
@@ -0,0 +1,21 @@
+var color = 'red';
+switch (color){
+  case 'blue':
+    console.log('blue')
+    break;
+  case 'red':
+    console.log('red')
+    break;
+  case 'yellow':
+    console.log('yellow')
+    break;
+  case 'green':
+    console.log('green')
+    break;
+  case 'white':
+    console.log('white')
+    break;
+  default:
+    console.log('black')
+    
+}
