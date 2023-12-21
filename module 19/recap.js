f413e6d902bba3f5bf060f50b2d9e4de8bfee29c
Nazia <nazia.jafrin1@gmail.com>
2023-12-21 20:48:20 +0600

    class 19-1

---
+++ module 19/recap.js
@@ -0,0 +1,14 @@
+var time = '10:15pm';
+var price = 150;
+var isWhiteColor = false;
+
+// array
+var partners = ['sajid', 'mojid', 'nojid'];
+var elementCount = partners.length;
+var nojidIndex = partners.indexOf('nojid');
+
+partners.push('kajit');
+partners.pop();
+
+partners.unshift('disa');
+partners.shift();
