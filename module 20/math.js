db90f51a8a984d72e48aa0d4eae9cf72986df24b
Nazia <nazia.jafrin1@gmail.com>
2023-12-25 13:33:57 +0600

    class 20-1

---
+++ module 20/math.js
@@ -0,0 +1,11 @@
+// let myNumber = -5;
+// console.log(Math.abs(myNumber))
+
+let myNumber = 19.5483
+console.log(Math.ceil(myNumber))
+console.log(Math.floor(myNumber))
+console.log(Math.round(myNumber))
+
+
+let output = Math.random() * 10
+console.log(output)
