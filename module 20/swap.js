7d3b66da7256f43f9742a77db1d014e6553e92b8
Nazia <nazia.jafrin1@gmail.com>
2023-12-28 15:04:28 +0600

    class 20-2

--- module 20/swap.js
+++ module 20/swap.js
@@ -2,7 +2,10 @@ var first = 4;
 var second = 5;
 console.log(first , second);
 
-var temp = first;
-first = second ;
-second = temp;
+// var temp = first;
+// first = second ;
+// second = temp;
+// console.log(first, second);
+
+[first, second] = [second, first]
 console.log(first, second);
