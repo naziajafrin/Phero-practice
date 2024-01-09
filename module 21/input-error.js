47aad2fc0f8287ef1f99e149ddac0afbe7b3067b
Nazia <nazia.jafrin1@gmail.com>
2024-01-09 20:43:39 +0600

    class 21-4

---
+++ module 21/input-error.js
@@ -0,0 +1,18 @@
+function add(num1, num2){
+  let sum = num1 + num2
+  return sum
+}
+
+let sum = add(12+3, 5)
+console.log(sum)
+
+
+function multiply(num1, num2){
+  let result = num1 * num2
+  return result
+}
+
+let result = multiply(12+3, 5)
+console.log(result)
+let result2 = multiply(13*12, 5)
+console.log(result2)
