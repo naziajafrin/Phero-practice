fd3855bad2f445675205db71378629747a5beece
Nazia <nazia.jafrin1@gmail.com>
2024-02-03 09:22:34 +0600

    problem 2

---
+++ module 21.5/variable_operations.js
@@ -0,0 +1,21 @@
+function addition(num1, num2){
+  let result = num1 + num2;
+  console.log(result);
+}
+function subtraction(num1, num2){
+  let result = num1 - num2;
+  console.log(result);
+}
+function multiply(num1, num2){
+  let result = num1 * num2;
+  console.log(result);
+}
+function division(num1, num2){
+  let result = num1 / num2;
+  console.log(result);
+}
+
+let sum = addition(5, 7);
+let difference = subtraction( 6, 7)
+let into = multiply(4 , 6)
+let divide = division(15, 3)
